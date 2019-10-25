83faaeed56786ceddf9e0e25d1f666cf2a5ce5e3
Djako Akpe Emmanuel <akpemanou@gmail.com>
2019-10-25 20:12:54 +0000

    Intent Changes updated

--- app/src/main/java/com/pluralsight/candycoded/MainActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/MainActivity.java
@@ -87,7 +87,6 @@ public class MainActivity extends AppCompatActivity {
         Intent infoIntent = new Intent(this, InfoActivity.class);
         startActivity(infoIntent);
         return super.onOptionsItemSelected(item);
-
     }
 
     // ***
